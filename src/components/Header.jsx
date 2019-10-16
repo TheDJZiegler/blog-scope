5c275e4eb5c42998031ea6f077704e91f7ed620f
alesdonoso <aledonosan@gmail.com>
2019-10-16 13:49:01 +0200

    fixed position of H1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Text = styled.div`
   color: ${props => props.theme.colors.white.base};
   z-index: 0;
   position: absolute;
-  top: 50%;
+  top: 70%;
   left: 50%;
   transform: translate(-50%, -50%);
   display: flex;
